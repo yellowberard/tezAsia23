c8074274b722013276a19b0cc340d4247b761d9c
Amari <44204724+amarikb@users.noreply.github.com>
2022-05-05 12:45:18 -0400

    testing in localhost

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 const io = new Server(server, {
   cors: {
-    origin: "https://uno-react.netlify.app",
+    origin: "http://localhost:3000", //"https://uno-react.netlify.app",
     methods: ["GET", "POST"],
   },
 });
