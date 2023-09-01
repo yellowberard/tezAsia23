affca5ceef3c6a3141b49c6c77b9f96fa19ff5e3
yellowberard <goelmanu002@gmail.com>
2023-09-01 21:12:31 +0530

    added filees

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
+const {
+  Server
+} = require("socket.io");
 const connectionHandler = require("./Connections/ConnectionHandler");
 
 app.use(cors());
@@ -10,13 +12,13 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 const io = new Server(server, {
   cors: {
-    origin: "https://uno-react.netlify.app",
+    // origin: "https://uno-react.netlify.app",
     methods: ["GET", "POST"],
   },
 });
 
 connectionHandler(io);
 
-server.listen(PORT, () => {
+server.listen(3002, () => {
   console.log("SERVER RUNNING");
 });
